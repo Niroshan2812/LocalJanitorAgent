0eb921575492027b4c2f263810fe1723425bcf20
Niroshan <niroshan9812@gmail.com>
2025-11-10 23:52:41 +0530

    Initial Commit

---
+++ LocalJanitorAgent/src/main/java/org/niroshan/localjanitoragent/LocalJanitorAgentApplication.java
@@ -0,0 +1,13 @@
+package org.niroshan.localjanitoragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalJanitorAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalJanitorAgentApplication.class, args);
+    }
+
+}
---
+++ LocalJanitorAgent/src/test/java/org/niroshan/localjanitoragent/LocalJanitorAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niroshan.localjanitoragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalJanitorAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
